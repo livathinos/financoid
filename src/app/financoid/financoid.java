1d25fd187d0e4dc1224ebe522f5a6da98b73a311
Spyros Livathinos <livathinos.spyros@gmail.com>
2010-07-23 18:49:11 +0300

    Project moved to eclipse platform

---
+++ src/app/financoid/financoid.java
@@ -0,0 +1,13 @@
+package app.financoid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class financoid extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
