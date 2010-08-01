0842d6385ee253a09f828ac4a182b0e12bc77710
Spyros Livathinos <livathinos.spyros@gmail.com>
2010-08-01 15:20:39 +0300

    Finished contact form. Pending integartion of functions

--- gen/app/financoid/R.java
+++ gen/app/financoid/R.java
@@ -8,16 +8,51 @@
 package app.financoid;
 
 public final class R {
+    public static final class array {
+        public static final int feedbacktypelist=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int ButtonSendFeedback=0x7f060007;
+        public static final int CheckBoxResponse=0x7f060006;
+        public static final int EditTextEmail=0x7f060003;
+        public static final int EditTextFeedbackBody=0x7f060005;
+        public static final int EditTextName=0x7f060002;
+        public static final int ScrollView01=0x7f060000;
+        public static final int SpinnerFeedbackType=0x7f060004;
+        public static final int TextViewTitle=0x7f060001;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int buttontext_email=0x7f050011;
+        public static final int buttontext_msg_pic=0x7f050012;
+        public static final int buttontext_simpleemail=0x7f05000d;
+        public static final int buttontext_simpleemail_chooser=0x7f050010;
+        public static final int chooser=0x7f05000e;
+        public static final int chooser_pic=0x7f05000f;
+        public static final int email_address=0x7f050015;
+        public static final int email_address_bcc=0x7f050017;
+        public static final int email_address_cc=0x7f050016;
+        public static final int email_message=0x7f050013;
+        public static final int email_subject=0x7f050014;
+        public static final int feedbackbody=0x7f050005;
+        public static final int feedbackbutton=0x7f050007;
+        public static final int feedbackemail=0x7f050004;
+        public static final int feedbackname=0x7f050003;
+        public static final int feedbackresponse=0x7f050006;
+        public static final int feedbacktitle=0x7f050002;
+        public static final int feedbacktype=0x7f050008;
+        public static final int feedbacktype1=0x7f050009;
+        public static final int feedbacktype2=0x7f05000a;
+        public static final int feedbacktype3=0x7f05000b;
+        public static final int feedbacktype4=0x7f05000c;
+        public static final int hello=0x7f050000;
     }
 }
