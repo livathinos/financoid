b90d71440e1ef1c2e2fef28184c2aa1cde39bca3
Spyros Livathinos <livathinos.spyros@gmail.com>
2010-08-01 19:51:34 +0300

    Added primitive menu screen at the beginning. Feedback function now working.

--- gen/app/financoid/R.java
+++ gen/app/financoid/R.java
@@ -9,7 +9,8 @@ package app.financoid;
 
 public final class R {
     public static final class array {
-        public static final int feedbacktypelist=0x7f040000;
+        public static final int feedbacktypelist=0x7f040001;
+        public static final int invitetypelist=0x7f040000;
     }
     public static final class attr {
     }
@@ -17,6 +18,7 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
+        public static final int ButtonLaunchFeedback=0x7f060008;
         public static final int ButtonSendFeedback=0x7f060007;
         public static final int CheckBoxResponse=0x7f060006;
         public static final int EditTextEmail=0x7f060003;
@@ -27,32 +29,48 @@ public final class R {
         public static final int TextViewTitle=0x7f060001;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int form=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f050001;
-        public static final int buttontext_email=0x7f050011;
-        public static final int buttontext_msg_pic=0x7f050012;
-        public static final int buttontext_simpleemail=0x7f05000d;
-        public static final int buttontext_simpleemail_chooser=0x7f050010;
-        public static final int chooser=0x7f05000e;
-        public static final int chooser_pic=0x7f05000f;
-        public static final int email_address=0x7f050015;
-        public static final int email_address_bcc=0x7f050017;
-        public static final int email_address_cc=0x7f050016;
-        public static final int email_message=0x7f050013;
-        public static final int email_subject=0x7f050014;
+        public static final int buttontext_email=0x7f050012;
+        public static final int buttontext_msg_pic=0x7f050013;
+        public static final int buttontext_simpleemail=0x7f05000e;
+        public static final int buttontext_simpleemail_chooser=0x7f050011;
+        public static final int chooser=0x7f05000f;
+        public static final int chooser_pic=0x7f050010;
+        public static final int email_address=0x7f050016;
+        public static final int email_address_bcc=0x7f050018;
+        public static final int email_address_cc=0x7f050017;
+        public static final int email_message=0x7f050014;
+        public static final int email_subject=0x7f050015;
         public static final int feedbackbody=0x7f050005;
-        public static final int feedbackbutton=0x7f050007;
+        public static final int feedbackbutton=0x7f050008;
         public static final int feedbackemail=0x7f050004;
+        public static final int feedbackmessagebody_format=0x7f050023;
+        public static final int feedbackmessagebody_responseno=0x7f050025;
+        public static final int feedbackmessagebody_responseyes=0x7f050024;
+        public static final int feedbackmessagesubject_format=0x7f050026;
         public static final int feedbackname=0x7f050003;
         public static final int feedbackresponse=0x7f050006;
+        public static final int feedbackresponsecheck=0x7f050007;
         public static final int feedbacktitle=0x7f050002;
-        public static final int feedbacktype=0x7f050008;
-        public static final int feedbacktype1=0x7f050009;
-        public static final int feedbacktype2=0x7f05000a;
-        public static final int feedbacktype3=0x7f05000b;
-        public static final int feedbacktype4=0x7f05000c;
+        public static final int feedbacktype=0x7f050009;
+        public static final int feedbacktype1=0x7f05000a;
+        public static final int feedbacktype2=0x7f05000b;
+        public static final int feedbacktype3=0x7f05000c;
+        public static final int feedbacktype4=0x7f05000d;
         public static final int hello=0x7f050000;
+        public static final int invite_activity_title=0x7f05001c;
+        public static final int invite_email_hint=0x7f05001f;
+        public static final int invite_email_label=0x7f05001a;
+        public static final int invite_message_preview_title=0x7f050019;
+        public static final int invitetype1=0x7f050020;
+        public static final int invitetype2=0x7f050021;
+        public static final int invitetype3=0x7f050022;
+        public static final int invitetypeprompt=0x7f05001e;
+        public static final int message_preview=0x7f05001d;
+        public static final int pick_email_label=0x7f05001b;
     }
 }
--- src/app/financoid/financoid.java
+++ src/app/financoid/financoid.java
@@ -1,89 +1,25 @@
 package app.financoid;
 
-import android.app.*;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class financoid extends Activity {
+public class Financoid extends Activity {
+
     /** Called when the activity is first created. */
-	//private static final int NOTIF_ID = 1234;
-	
-	@Override
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        /*Alert dialogue example*/
-        
-        /*new AlertDialog.Builder(this)
-        .setTitle("Popups rule")
-        .setMessage("Close me!")
-        .setPositiveButton("OK",null)
-        .setNeutralButton("Close",null)
-        .setNegativeButton("Not OK",null)
-        .setIcon(R.drawable.icon)
-        .show();*/
-        
-        /*Toast popup example*/
-        
-        /*Toast.makeText(this,"Your download has resumed",Toast.LENGTH_LONG).show();*/
-        
-        /*Notification message example*/
-        
-        
-        /*
-        NotificationManager notifManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        Notification note = new Notification(R.drawable.icon, "New E-mail", System.currentTimeMillis());
-        
-        PendingIntent intent = PendingIntent.getActivity(this,0,new Intent(this, financoid.class),0);
-        
-        note.setLatestEventInfo(this, "New E-mail","You have one unread message.", intent);
-        
-        notifManager.notify(NOTIF_ID, note);*/
-        
     }
-	
-	public void sendSimpleEmail(View button) {
-		
-		Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
-		emailIntent.setType("plain/text");
-		startActivity(emailIntent);
-		
-	}
-	
-	public void sendPlainTextEmail(View button) {
-		
-		Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
-		
-		String aEmailList[] = { getResources()
-				.getString(R.string.email_address) };
-		String aEmailCCList[] = { getResources()
-				.getString(R.string.email_address_cc) };
-		String aEmailBCCList[] = { getResources()
-				.getString(R.string.email_address_bcc) };
-		
-		emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, aEmailList);
-		emailIntent.putExtra(android.content.Intent.EXTRA_CC, aEmailCCList);
-		emailIntent.putExtra(android.content.Intent.EXTRA_BCC, aEmailBCCList);
-		emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
-				getResources().getString(R.string.email_subject));
+    
+    /*No menu yet, we just launch the form layout.*/
+    public void launchForm(View button) {
 
-		emailIntent.setType("plain/text");
-		emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, getResources()
-				.getString(R.string.email_message));
+        Intent launchFormActivity = new Intent(this, FormActivity.class); 
+        startActivity(launchFormActivity);
 
-		startActivity(emailIntent);
-		
-	}
-	
-	public void sendFeedback(View button) {
+    }
 
-		Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
-		emailIntent.setType("plain/text");
-		startActivity(Intent.createChooser(emailIntent, getResources().getString(R.string.chooser)));
-
-	}
-	
-	
 }
